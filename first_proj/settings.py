a4bd580819072eec9640cfbc542ac847d73cadb0
sirdesmond09 <sirdesmond09@gmail.com>
2022-04-30 12:49:51 +0100

    cors error

--- first_proj/settings.py
+++ first_proj/settings.py
@@ -51,11 +51,13 @@ INSTALLED_APPS = [
     "coreapi",
     'drf_yasg',
     "djoser",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -199,3 +201,15 @@ SWAGGER_SETTINGS = {
         }
         }
     }
+
+
+#Cors headers
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:9000",
+# ]
